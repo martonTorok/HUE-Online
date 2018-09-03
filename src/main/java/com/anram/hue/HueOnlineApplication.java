7a92385d454b76bf7313fbdc5842154cd271cd0f
martonTorok <tormartonz@gmail.com>
2018-09-03 12:55:33 +0200

    basic file structure created

---
+++ src/main/java/com/anram/hue/HueOnlineApplication.java
@@ -0,0 +1,12 @@
+package com.anram.hue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HueOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HueOnlineApplication.class, args);
+    }
+}
---
+++ src/test/java/com/anram/hue/hue/HueOnlineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anram.hue.hue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HueOnlineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
